ed52415116b8f2baeae94fc0cd71a29d2a1583a5
leon <leoncyao@gmail.com>
2023-11-16 17:09:59 -0500

    added email sending script

---
+++ compile_pdf.sh
@@ -0,0 +1,2 @@
+xelatex -interaction nonstopmode -halt-on-error -file-line-error \
+    '\newcommand{\positionnamevalue}{Software Engineer 1} \newcommand{\companynamevalue}{Wayfair} \input Cover_Letter'
